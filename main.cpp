df255eb7cee13b0988227b0b8fd2c726c356f0ca
TwShrimp <tedaventador@gmail.com>
2026-09-08 02:07:23 -0400

    Add battery level printing function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+void printBattery(int batteryPercent) {
+  std::cout << "Battery Level: " << batteryPercent << "%" << std:: endl;
+
+  if (batteryPercent < 20) {
+    std::cout << "Warning: Low battery" << std::endl;
+  }
+}
+
 int main() {
   std::cout << "Hello World" << std::endl;
+  printBattery(75);
+  
   return 0;
 }
